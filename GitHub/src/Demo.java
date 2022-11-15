e840bc58195754fd9ca66cd29833fd4897bf271c
balasahebshinde <balasahebshindepatil@gmail.com>
2022-11-15 19:26:23 +0530

    githib1

---
+++ GitHub/src/Demo.java
@@ -0,0 +1,12 @@
+
+public class Demo {
+	public static void main(String[] args) {
+		String org="RUTU";
+		String rev="";
+		for(int i=org.length()-1;i>=0;i--) {
+			rev=rev+org.charAt(i);
+		}
+		System.out.println(rev);
+	}
+
+}
